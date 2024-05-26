f6ad3105e90c30e09bf1e444d6fec8faf9f36554
tttom3669 <tttom3669@gmail.com>
2024-05-26 15:45:51 +0800

    Updates

---
+++ assets/index-BKxJ4BSf.js
@@ -0,0 +1,1 @@
+import"./vite.entry-DsyvOBC9.js";new Swiper(".swiper-article",{slidesPerView:1,breakpoints:{768:{slidesPerView:2},992:{slidesPerView:3}},spaceBetween:24,pagination:{el:".swiper-pagination"}});
