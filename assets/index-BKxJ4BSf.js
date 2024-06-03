fefcc203f47237abaea4825e8bc9f3ef7d975914
tttom3669 <tttom3669@gmail.com>
2024-06-03 21:58:26 +0800

    Updates

--- assets/index-BKxJ4BSf.js
+++
@@ -1,1 +0,0 @@
-import"./vite.entry-DsyvOBC9.js";new Swiper(".swiper-article",{slidesPerView:1,breakpoints:{768:{slidesPerView:2},992:{slidesPerView:3}},spaceBetween:24,pagination:{el:".swiper-pagination"}});
---
+++ assets/swiper-DgOBFxNZ.js
@@ -0,0 +1,1 @@
+new Swiper(".swiper-article",{slidesPerView:1,breakpoints:{768:{slidesPerView:2},992:{slidesPerView:3}},spaceBetween:24,pagination:{el:".swiper-pagination"}});
